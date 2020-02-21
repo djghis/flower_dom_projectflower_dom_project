91fe18c54128caa87c5798893fd44a28e5ab175b
djghis <djghis@hotmail.com>
2020-02-21 15:11:20 +0000

    code refactored to lab solution

--- js/app.js
+++ js/app.js
@@ -1,24 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#new-item-form');
-  form.addEventListener('submit', handleFormSubmit);
+  const newItemform = document.querySelector('#new-item-form');
+  newItemform.addEventListener('submit', handleFormSubmit);
 
-  const button = document.createElement('button');
-  const body = document.querySelector('body');
-  body.appendChild(button);
-  button.textContent = 'Delete All';
-  button.addEventListener('click', handleButtonClick);
+  // const deleteAllButton = document.createElement('button');
+  // const body = document.querySelector('body');
+  // body.appendChild(deleteAllButton);
+  // deleteAllButton.textContent = 'Delete All';
+  // deleteAllButton.addEventListener('click', handleDeleteButtonClick);
 });
 
-const handleButtonClick = function () {
-  const stock = document.querySelector('ul');
-  stock.innerHTML = '';
-};
+// const handleDeleteButtonClick = function () {
+//   const stock = document.querySelector('ul');
+//   stock.innerHTML = '';
+// };
 
-const handleFormSubmit = function () {
+const handleFormSubmit = function (event) {
   event.preventDefault();
-  const newItem = document.createElement('li');
-  const item = document.querySelector('ul');
-  item.appendChild(newItem);
-  newItem.textContent = `Name: ${event.target.name.value}, Description: ${event.target.description.value}, Category: ${event.target.category.value}.`
+
+  const stockListItem = createStockListItem(event.target);
+  const stockList = document.querySelector('#stock');
+  stockList.appendChild(stockListItem);
+
   event.target.reset();
 };
+
+const createStockListItem = function (form) {
+  const stockListItem = document.createElement('li');
+  stockListItem.classList.add('stock-list-item');
+
+  const category = document.createElement('h2');
+  category.textContent = form.category.value;
+  stockListItem.appendChild(category);
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  stockListItem.appendChild(name);
+
+  const description = document.createElement('h3');
+  description.textContent = form.description.value;
+  stockListItem.appendChild(description);
+
+
+
+
+  return stockListItem;
+
+};
+
+
+
+// const handleFormSubmit = function () {
+//   event.preventDefault();
+//   const newItem = document.createElement('li');
+//   const list = document.querySelector('ul');
+//   list.appendChild(newItem);
+//   newItem.textContent = `Name: ${event.target.name.value}, Description: ${event.target.description.value}, Category: ${event.target.category.value}.`
+//   event.target.reset();
+// };
